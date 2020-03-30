a63594f076d47b9a6a710310f64658ff21b13609
NiteshChauhan430 <50042119+NiteshChauhan430@users.noreply.github.com>
2020-03-30 19:51:33 +0530

    SpringSecurityBasicApplicatin

---
+++ SpringBootSecurityBasic/SpringBootSecurityBasic/src/main/java/com/example/Main()/SpringBootSecurityBasicApplication.java
@@ -0,0 +1,14 @@
+package com.example.SpringBootSecurityBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityBasicApplication.class, args);
+		System.Out.println("This is Spring Security);
+	}
+
+}
